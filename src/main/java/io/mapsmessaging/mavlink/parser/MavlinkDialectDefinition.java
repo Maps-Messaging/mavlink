0f984a2a2f09a1497cef9065f6c93c91cf20b313
Matthew Buckton <matthew@buckton.org>
2026-01-05 16:19:33 +1100

    update copyright

--- src/main/java/io/mapsmessaging/mavlink/parser/MavlinkDialectDefinition.java
+++ src/main/java/io/mapsmessaging/mavlink/parser/MavlinkDialectDefinition.java
@@ -1,7 +1,7 @@
 /*
  *
  *  Copyright [ 2020 - 2024 ] Matthew Buckton
- *  Copyright [ 2024 - 2025 ] MapsMessaging B.V.
+ *  Copyright [ 2024 - 2026 ] MapsMessaging B.V.
  *
  *  Licensed under the Apache License, Version 2.0 with the Commons Clause
  *  (the "License"); you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- *
  */
 
 package io.mapsmessaging.mavlink.parser;
